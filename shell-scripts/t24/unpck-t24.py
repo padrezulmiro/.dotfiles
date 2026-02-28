64c555542cd373b42c6a48003615336afdde3854
aZul <fmdcosta.piano@gmail.com>
2026-02-28 18:11:46 +0000

    wip: Unpack script

--- shell-scripts/t24/unpck-t24.py
+++ shell-scripts/t24/unpck-t24.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python3
 
 import click
+import os
+from time import time_ns
+from tempfile import mkdtemp
 
 
 @click.command()
-@click.argument("source", default=".", type=click.Path())
+@click.argument("source", default=".", type=click.Path(dir_okay=False))
 @click.argument("destination", default=".", type=click.Path())
-def unpack(source: str, destination: str):
-    """
-    Unpack SOURCE T24 lib files, and store them in DESTINATION.
+def root_cmd(source: str, destination: str):
+    """Unpack SOURCE T24 lib files, and store them in DESTINATION.
 
+    \b
     SOURCE is a source lib or a directory containing libs.
     DESTINATION is the destination path to store the decompiled output.
     """
-    click.echo(source)
-    click.echo(destination)
-    pass
+    click.echo(f"Unpacking T24 Libs from {source} to {destination}")
 
+    # Review contents of JAR file
+    # Extract API class files in case a decompiled version doesn't exist already
+    # Decompile class files
 
-def unzip_files():
-    pass
+    src_ext = os.path.splitext(source)[1]
+    if src_ext != ".jar" and src_ext != ".class":
+        raise click.BadArgumentUsage("SOURCE has to be a JAR or CLASS file.")
 
+    tdir_abspath = mkdtemp(dir=".")
 
-def del_extra_class_files():
+def unpack_jar():
     pass
 
 def run_vineflower():
     pass
 
+
 if __name__ == '__main__':
-    unpack()
+    root_cmd()
